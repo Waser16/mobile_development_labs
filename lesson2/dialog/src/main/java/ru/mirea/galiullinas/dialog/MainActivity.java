a25c939fc6aefc177813b3ffc0f097f2ae471b79
Artur <galart82003@gmail.com>
2024-03-14 23:13:48 +0300

    Dialog: MyDialogFragment almost done

---
+++ lesson2/dialog/src/main/java/ru/mirea/galiullinas/dialog/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.mirea.galiullinas.dialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickShowDialog(View view) {
+        MyDialogFragment dialogFragment = new MyDialogFragment();
+        dialogFragment.show(getSupportFragmentManager(), "mirea");
+    }
+}
---
+++ lesson2/dialog/src/main/java/ru/mirea/galiullinas/dialog/MyDialogFragment.java
@@ -0,0 +1,28 @@
+package ru.mirea.galiullinas.dialog;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class MyDialogFragment extends DialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Здравтсвуй МИРЭА!")
+                .setMessage("Успех близок?")
+                .setIcon(R.mipmap.ic_launcher_round)
+                .setPositiveButton("Иду дальше", new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id) {
+                        // TODO: дописать
+                    }
+                });
+        return builder.create();
+    }
+}
