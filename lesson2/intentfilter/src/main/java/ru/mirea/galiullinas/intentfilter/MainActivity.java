eb30d6bbafc88a2dd5da89a68b256ae42e1a2e16
Artur <galart82003@gmail.com>
2024-03-14 21:51:57 +0300

    IntentFilter done

---
+++ lesson2/intentfilter/src/main/java/ru/mirea/galiullinas/intentfilter/MainActivity.java
@@ -0,0 +1,34 @@
+package ru.mirea.galiullinas.intentfilter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBrowserIntent(View view) {
+        Uri address = Uri.parse("https://www.mirea.ru/");
+        Intent openLinkIntent = new Intent(Intent.ACTION_VIEW, address);
+        startActivity(openLinkIntent);
+    }
+
+    public void sendInfoToOtherApp(View view) {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_SUBJECT, "MIREA");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, "Галиуллин Артур Шамилевич");
+        startActivity(shareIntent);
+    }
+
+
+}
