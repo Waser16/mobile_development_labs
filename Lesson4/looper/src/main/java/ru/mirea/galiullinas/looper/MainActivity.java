241f598faa274957e9eb4b516e43fed1abecc421
galart <galart82003@gmail.com>
2024-04-15 22:38:23 +0300

    Looper done

--- Lesson4/looper/src/main/java/ru/mirea/galiullinas/looper/MainActivity.java
+++ Lesson4/looper/src/main/java/ru/mirea/galiullinas/looper/MainActivity.java
@@ -1,12 +1,20 @@
 package ru.mirea.galiullinas.looper;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
 
 import ru.mirea.galiullinas.looper.databinding.ActivityMainBinding;
 
@@ -18,5 +26,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Handler mainThreadHandler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                Log.d(MainActivity.class.getSimpleName(), "Task execute. This is result: " + msg.getData().getString("result"));
+            }
+        };
+        MyLooper myLooper = new MyLooper(mainThreadHandler);
+        myLooper.start();
+        binding.editTextText.setText("Мой номер по списку №___");
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message msg = Message.obtain();
+                Bundle bundle = new Bundle();
+                int age = Integer.parseInt(binding.editTextAge.getText().toString());
+                String work = binding.editTextWork.getText().toString();
+
+                @SuppressLint("DefaultLocale")
+                String result = String.format("ВОЗРАСТ: %d, ДОЛЖНОСТЬ: %s", age, work);
+                bundle.putString("KEY", result);
+                bundle.putInt("age", age);
+                msg.setData(bundle);
+                myLooper.mHandler.sendMessage(msg);
+            }
+            });
     }
 }
