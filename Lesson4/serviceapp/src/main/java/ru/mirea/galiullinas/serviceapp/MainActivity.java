f5a27af2df09f7bbb5000ed7c5e9d4720626bab5
galart <galart82003@gmail.com>
2024-04-17 21:48:50 +0300

    ServiceApp almost done

--- Lesson4/serviceapp/src/main/java/ru/mirea/galiullinas/serviceapp/MainActivity.java
+++ Lesson4/serviceapp/src/main/java/ru/mirea/galiullinas/serviceapp/MainActivity.java
@@ -1,18 +1,58 @@
 package ru.mirea.galiullinas.serviceapp;
 
+import static android.Manifest.permission.FOREGROUND_SERVICE;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import ru.mirea.galiullinas.serviceapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
+    private int permissionCode = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        if (ContextCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED) {
+            Log.d(MainActivity.class.getSimpleName().toString(), "Разрешения получены");
+        }
+        else {
+            Log.d(MainActivity.class.getSimpleName().toString(), "Разрешений не получено");
+            ActivityCompat.requestPermissions(this, new String[] {POST_NOTIFICATIONS, FOREGROUND_SERVICE},
+                    permissionCode);
+        }
+
+        binding.btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent serviceIntent = new Intent(MainActivity.this, PlayerService.class);
+                ContextCompat.startForegroundService(MainActivity.this, serviceIntent);
+            }
+        });
+
+        binding.btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, PlayerService.class));
+            }
+        });
+
     }
 }
--- Lesson4/serviceapp/src/main/java/ru/mirea/galiullinas/serviceapp/PlayerService.java
+++ Lesson4/serviceapp/src/main/java/ru/mirea/galiullinas/serviceapp/PlayerService.java
@@ -1,16 +1,67 @@
 package ru.mirea.galiullinas.serviceapp;
 
+import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.IBinder;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
 public class PlayerService extends Service {
+
+    private MediaPlayer mediaPlayer;
+    public static final String CHANNEL_ID = "ForegroundServiceChannel";
+
     public PlayerService() {
+
     }
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                stopForeground(true);
+            }
+        });
+        return super.onStartCommand(intent, flags, startId);
+
+    }
+    @SuppressLint("ForegroundServiceType")
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentText("Playing...")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("best player"))
+                .setContentTitle("Music Player");
+
+        int importance = NotificationManager.IMPORTANCE_DEFAULT;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Galiullin AS notification",
+                importance);
+        channel.setDescription("MIREA Channel");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+        notificationManager.createNotificationChannel(channel);
+        startForeground(1, builder.build());
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.underdog);
+        mediaPlayer.setLooping(false);
+    }
+
+    public void onDestroy() {
+        stopForeground(true);
+        mediaPlayer.stop();
     }
 }
