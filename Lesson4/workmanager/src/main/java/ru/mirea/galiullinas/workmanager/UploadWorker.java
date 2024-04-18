569cee0e97355c15afb953e1d6979a16aba99d27
galart <galart82003@gmail.com>
2024-04-18 21:06:14 +0300

    workmanager done

--- Lesson4/workmanager/src/main/java/ru/mirea/galiullinas/workmanager/UploadWorker.java
+++ Lesson4/workmanager/src/main/java/ru/mirea/galiullinas/workmanager/UploadWorker.java
@@ -1,10 +1,13 @@
 package ru.mirea.galiullinas.workmanager;
 
 import android.content.Context;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
+
+import java.util.concurrent.TimeUnit;
 
 public class UploadWorker extends Worker {
     static final String TAG = "UploadWorker";
@@ -16,6 +19,14 @@ public class UploadWorker extends Worker {
     @Override
     public Result doWork() {
         Log.d(TAG, "doWork: start");
-        try { }
+        try {
+            TimeUnit.SECONDS.sleep(10);
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        Log.d(TAG, "doWork: end");
+        return Result.success();
     }
 }
