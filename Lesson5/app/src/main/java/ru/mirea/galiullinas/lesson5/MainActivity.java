2fa01b6f1823d6e00f8178069362b791fa602ef5
galart <galart82003@gmail.com>
2024-04-22 14:12:11 +0300

    App done

--- Lesson5/app/src/main/java/ru/mirea/galiullinas/lesson5/MainActivity.java
+++ Lesson5/app/src/main/java/ru/mirea/galiullinas/lesson5/MainActivity.java
@@ -1,6 +1,13 @@
 package ru.mirea.galiullinas.lesson5;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,11 +15,57 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
+import ru.mirea.galiullinas.lesson5.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensors = sensorManager.getSensorList(Sensor.TYPE_PRESSURE);
+        ListView listView = binding.listView;
+
+        ArrayList<HashMap<String, Object>> arrayList = new ArrayList<>();
+        for (int i = 0; i < sensors.size(); i++) {
+            HashMap<String, Object> sensorTypeList = new HashMap<>();
+            sensorTypeList.put("Name", sensors.get(i).getName());
+            sensorTypeList.put("Value", sensors.get(i).getMaximumRange());
+            arrayList.add(sensorTypeList);
+        }
+
+        SimpleAdapter mHistory =
+                new SimpleAdapter(this, arrayList, android.R.layout.simple_list_item_2,
+                        new String[] {"Name", "Value"},
+                        new int[] {android.R.id.text1, android.R.id.text2});
+        listView.setAdapter(mHistory);
+
+        Sensor accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometerSensor,
+                sensorManager.SENSOR_DELAY_NORMAL);
+
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float valueAzimuth = event.values[0];
+            float valuePitch = event.values[1];
+            float valueRoll = event.values[2];
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
