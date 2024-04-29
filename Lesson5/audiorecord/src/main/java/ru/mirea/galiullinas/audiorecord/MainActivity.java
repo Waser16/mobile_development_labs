8e25905847983543b357d3cca0d6ad819f2870a3
galart <galart82003@gmail.com>
2024-04-29 17:57:27 +0300

    audiorecorder done

--- Lesson5/audiorecord/src/main/java/ru/mirea/galiullinas/audiorecord/MainActivity.java
+++ Lesson5/audiorecord/src/main/java/ru/mirea/galiullinas/audiorecord/MainActivity.java
@@ -2,53 +2,147 @@ package ru.mirea.galiullinas.audiorecord;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import java.security.Permission;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
 
 import ru.mirea.galiullinas.audiorecord.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = MainActivity.class.getSimpleName();
-    private static final int REQUEST_CODE_PERMISSION = 100;
-
-    private boolean isWork;
+    private final String TAG = MainActivity.class.getSimpleName();
     private ActivityMainBinding binding;
+    private static final int REQUEST_CODE_PERMISSION = 200;
+    private boolean isWork;
+    private String fileName = null;
+    private Button recordButton = null;
+    private Button playButton = null;
+    private MediaRecorder recorder = null;
+    private MediaPlayer player = null;
+    boolean isStartRecording = true;
+    boolean isStartPlaying = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        recordButton = binding.recordButton;
+        playButton = binding.playButton;
+        playButton.setEnabled(false);
+        fileName = (new File(getExternalFilesDir(Environment.DIRECTORY_MUSIC),
+                "/audiorecordtest.3gp")).getAbsolutePath();
 
-        int audioRecordPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
-        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (audioRecordPermissionStatus == PackageManager.PERMISSION_GRANTED &&
-            storagePermissionStatus == PackageManager.PERMISSION_GRANTED) {
+        int audioRecordPermissionStatus = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.RECORD_AUDIO);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if(audioRecordPermissionStatus == PackageManager.PERMISSION_GRANTED &&
+                storagePermissionStatus == PackageManager.PERMISSION_GRANTED) {
             isWork = true;
-            Log.d(TAG, "PermissionsGranted");
-        }
-        else {
-            requestPermissions(new String[] {Manifest.permission.RECORD_AUDIO ,Manifest.permission.WRITE_EXTERNAL_STORAGE},
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]
+                            {Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE},
                     REQUEST_CODE_PERMISSION);
-            Log.d(TAG, "Permissions requested");
         }
+
+
+
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isStartRecording) {
+                    recordButton.setText("Stop recording");
+                    playButton.setEnabled(false);
+                    startRecording();
+                } else {
+                    recordButton.setText("Start recording");
+                    playButton.setEnabled(true);
+                    stopRecording();
+                }
+                isStartRecording = !isStartRecording;
+            }
+        });
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isStartPlaying) {
+                    playButton.setText("Stop playing");
+                    recordButton.setEnabled(false);
+                    startPlaying();
+                } else {
+                    playButton.setText("Start playing");
+                    recordButton.setEnabled(true);
+                    stopPlaying();
+                }
+                isStartPlaying = !isStartPlaying;
+            }
+        });
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch(requestCode) {
+            case REQUEST_CODE_PERMISSION:
+                isWork = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                break;
+        }
 
-        isWork = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED;
+        Log.d(TAG, Arrays.toString(grantResults) + " grantResults");
+        Log.d(TAG, Arrays.toString(permissions) + " permissions");
 
+        if(!isWork) finish();
+    }
+
+    private void startRecording() {
+        recorder = new MediaRecorder();
+        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        recorder.setOutputFile(fileName);
+        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+        try {
+            recorder.prepare();
+        } catch (IOException e) {
+            Log.e(TAG, "startRecording error");
+        }
+        recorder.start();
+    }
+
+    private void stopRecording() {
+        recorder.stop();
+        recorder.release();
+        recorder = null;
+    }
+
+    private void startPlaying() {
+        player = new MediaPlayer();
+        try {
+            player.setDataSource(fileName);
+            player.prepare();
+            player.start();
+        } catch (IOException e) {
+            Log.e(TAG, "startPlaying error");
+        }
+    }
+
+    private void stopPlaying() {
+        player.release();
+        player = null;
     }
 }
