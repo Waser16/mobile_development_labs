0eb9e0160c18662037e3e52c1abdffec4a9eee3d
galart <galart82003@gmail.com>
2024-05-03 18:30:29 +0300

    Permissions added

--- MireaProject/app/src/main/java/ru/mirea/galiullinas/mireaproject/MainActivity.java
+++ MireaProject/app/src/main/java/ru/mirea/galiullinas/mireaproject/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.mirea.galiullinas.mireaproject;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
@@ -7,12 +8,15 @@ import android.view.Menu;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
 
 import ru.mirea.galiullinas.mireaproject.databinding.ActivityMainBinding;
 
@@ -20,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
+    public boolean isWork = false;
+    private int REQUEST_CODE_PERMISSION = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,5 +67,35 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+    private void checkPermissions() {
+        int internetPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int audioPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
+        int cameraPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA);
+        int storageReadPermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int storagePermissionStatus = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (cameraPermissionStatus == PackageManager.PERMISSION_GRANTED && storagePermissionStatus
+                == PackageManager.PERMISSION_GRANTED && storageReadPermissionStatus == PackageManager.PERMISSION_GRANTED
+                && audioPermissionStatus == PackageManager.PERMISSION_GRANTED && internetPermissionStatus == PackageManager.PERMISSION_GRANTED
+        ) {
+            isWork = true;
+        } else {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA,
+                    Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.INTERNET
+                    }, REQUEST_CODE_PERMISSION
+            );
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult( int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSION) {
+            isWork = grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED;
+        }
     }
 }
