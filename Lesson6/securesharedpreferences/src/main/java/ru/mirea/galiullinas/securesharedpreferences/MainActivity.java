756b832b3033e75f916ef51a5a992723ea450dd9
galart <galart82003@gmail.com>
2024-05-04 18:40:32 +0300

    SecureSharedPreferences done

---
+++ Lesson6/securesharedpreferences/src/main/java/ru/mirea/galiullinas/securesharedpreferences/MainActivity.java
@@ -0,0 +1,47 @@
+package ru.mirea.galiullinas.securesharedpreferences;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKeys;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import ru.mirea.galiullinas.securesharedpreferences.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        KeyGenParameterSpec keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC;
+        try {
+            String mainKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec);
+            SharedPreferences secureSharedPreferences = EncryptedSharedPreferences.create(
+                    "secret_shared_prefs",
+                    mainKeyAlias,
+                    getBaseContext(),
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+            secureSharedPreferences.edit().putString("secure", "Том Хэнкс").apply();
+            binding.editTextFavACtor.setText(secureSharedPreferences.getString("secure", "unknown"));
+        } catch (GeneralSecurityException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
