b10b0b77bb757d3ce0167d0990a45da161d7db77
galart <galart82003@gmail.com>
2024-05-05 13:13:07 +0300

    EmployeeDB done

--- Lesson6/employeedb/src/main/java/ru/mirea/galiullinas/employeedb/MainActivity.java
+++ Lesson6/employeedb/src/main/java/ru/mirea/galiullinas/employeedb/MainActivity.java
@@ -1,6 +1,7 @@
 package ru.mirea.galiullinas.employeedb;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,35 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.List;
+
 import ru.mirea.galiullinas.employeedb.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private static final String TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        AppDatabase db = App.getInstance().getDatabase();
+        EmployeeDao employeeDao = db.employeeDao();
 
+        Employee employee = new Employee();
+
+        employee.id = 1;
+        employee.name = "John Smith";
+        employee.salary = 10000;
+
+        employeeDao.insert(employee);
+
+        List<Employee> employeeList = employeeDao.getAll();
+        employee = employeeDao.getById(1);
+        employee.salary = 20000;
+        employeeDao.update(employee);
+        Log.d(TAG, employee.name + " " + employee.salary);
     }
 }
