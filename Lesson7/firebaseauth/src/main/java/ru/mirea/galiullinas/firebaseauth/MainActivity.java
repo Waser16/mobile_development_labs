79d7a84ee0aba9c995e268ce4a192451b89a2328
galart <galart82003@gmail.com>
2024-05-09 17:28:29 +0300

    FirebaseAuth done

---
+++ Lesson7/firebaseauth/src/main/java/ru/mirea/galiullinas/firebaseauth/MainActivity.java
@@ -0,0 +1,165 @@
+package ru.mirea.galiullinas.firebaseauth;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+import ru.mirea.galiullinas.firebaseauth.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private ActivityMainBinding binding;
+    // START declare_auth
+    private FirebaseAuth mAuth;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+// Initialization views
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // [START initialize_auth] Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+        binding.verifyEmailButton.setOnClickListener(v -> sendEmailVerification());
+        binding.createAccButton.setOnClickListener(v -> createAccount(
+                binding.emailField.getText().toString(),
+                binding.passwordField.getText().toString()));
+        binding.signInButton.setOnClickListener(v -> signIn(
+                binding.emailField.getText().toString(),
+                binding.passwordField.getText().toString()
+        ));
+        binding.signOutButton.setOnClickListener(v -> signOut());
+    }
+    // [START on_start_check_user]
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    // [END on_start_check_user]
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            binding.statusTextView.setText(getString(R.string.emailpassword_status_fmt,
+                    user.getEmail(), user.isEmailVerified()));
+            binding.detailTextView.setText(getString(R.string.firebase_status_fmt, user.getUid()));
+            binding.authTable.setVisibility(View.GONE);
+            binding.signedTable.setVisibility(View.VISIBLE);
+            binding.verifyEmailButton.setEnabled(!user.isEmailVerified());
+        } else {
+            binding.statusTextView.setText(R.string.signed_out);
+            binding.detailTextView.setText(null);
+            binding.authTable.setVisibility(View.VISIBLE);
+            binding.signedTable.setVisibility(View.GONE);
+        }
+    }
+
+private void createAccount(String email, String password) {
+        Log.d(TAG, "createAccount:" + email);
+//        if (!validateForm()) {
+//            return;
+//        }
+// [START create_user_with_email]
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+// Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+// If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure",
+                                    task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+// [END create_user_with_email]
+    }
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user'sinformation
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            binding.statusTextView.setText(R.string.auth_failed);
+                        }
+// [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
+    }
+
+    private void signOut() {
+        mAuth.signOut();
+        updateUI(null);
+    }
+
+    private void sendEmailVerification() {
+        // Disable button
+        binding.verifyEmailButton.setEnabled(false);
+        // Send verification email
+        // [START send_email_verification]
+        final FirebaseUser user = mAuth.getCurrentUser();
+        Objects.requireNonNull(user).sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // [START_EXCLUDE]
+                        // Re-enable button
+                        binding.verifyEmailButton.setEnabled(true);
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this,
+                                    "Verification email sent to " + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Log.e(TAG, "sendEmailVerification", task.getException());
+                            Toast.makeText(MainActivity.this,
+                                    "Failed to send verification email.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+// [END_EXCLUDE]
+                    }
+                });
+        // [END send_email_verification]
+    }
+
+}
