3222b50775b5bf36fa456a418897b18584316da0
galart <galart82003@gmail.com>
2024-05-10 13:43:29 +0300

    Lesson8 init

---
+++ Lesson8/YandexMaps/src/main/java/ru/mirea/galiullinas/lesson8/MainActivity.java
@@ -0,0 +1,22 @@
+package ru.mirea.galiullinas.lesson8;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import ru.mirea.galiullinas.lesson8.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
