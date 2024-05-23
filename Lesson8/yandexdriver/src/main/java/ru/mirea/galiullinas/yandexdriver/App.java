6424e90b921231413b6e11a99d9ff7960f55b5ee
galart <galart82003@gmail.com>
2024-05-23 22:38:41 +0300

    Yandexdriver done

---
+++ Lesson8/yandexdriver/src/main/java/ru/mirea/galiullinas/yandexdriver/App.java
@@ -0,0 +1,15 @@
+package ru.mirea.galiullinas.yandexdriver;
+
+import android.app.Application;
+
+import com.yandex.mapkit.MapKitFactory;
+
+public class App extends Application {
+    private final String MAPKIT_API_KEY = "03a77017-e5f1-4277-a657-8e4495e21423";
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // Set the api key before calling initialize on MapKitFactory.
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+    }
+}
