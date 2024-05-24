99bee317f5b7f8acedbdf109e0a47f0393e04ca5
galart <galart82003@gmail.com>
2024-05-24 19:52:39 +0300

    MireaProject done

--- MireaProject/app/src/main/java/ru/mirea/galiullinas/mireaproject/ui/map/MapActivity.java
+++ MireaProject/app/src/main/java/ru/mirea/galiullinas/mireaproject/ui/map/MapActivity.java
@@ -13,18 +13,18 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import com.yandex.mapkit.Animation;
-import com.yandex.mapkit.GeoObject;
 import com.yandex.mapkit.MapKit;
 import com.yandex.mapkit.MapKitFactory;
 import com.yandex.mapkit.layers.GeoObjectTapEvent;
 import com.yandex.mapkit.layers.GeoObjectTapListener;
+import com.yandex.mapkit.map.GeoObjectSelectionMetadata;
+import com.yandex.mapkit.map.InputListener;
+import com.yandex.mapkit.map.Map;
 import com.yandex.mapkit.layers.ObjectEvent;
 import com.yandex.mapkit.map.CameraPosition;
 import com.yandex.mapkit.map.CompositeIcon;
-import com.yandex.mapkit.map.GeoObjectSelectionMetadata;
 import com.yandex.mapkit.map.IconStyle;
 import com.yandex.mapkit.map.InputListener;
-import com.yandex.mapkit.map.Map;
 import com.yandex.mapkit.map.RotationType;
 import com.yandex.mapkit.mapview.MapView;
 import com.yandex.mapkit.geometry.Point;
@@ -55,10 +55,56 @@ public class MapActivity extends AppCompatActivity implements
     private UserLocationLayer userLocationLayer;
     private boolean isWork = false;
     private final int REQUEST_CODE_PERMISSION = 100;
-
     private SearchManager searchManager;
     public Session searchSession;
-    private GeoObjectTapListener tapListener;
+    private GeoObjectTapListener tapListener = new GeoObjectTapListener() {
+        @Override
+        public boolean onObjectTap(@NonNull GeoObjectTapEvent geoObjectTapEvent) {
+            GeoObjectSelectionMetadata selectionMetadata = geoObjectTapEvent.getGeoObject()
+                    .getMetadataContainer().getItem(GeoObjectSelectionMetadata.class);
+            binding.mapView.getMap().selectGeoObject(selectionMetadata.getId(), selectionMetadata.getLayerId());
+            return false;
+        }
+    };;
+    private Session.SearchListener searchListener = new Session.SearchListener() {
+        @Override
+        public void onSearchResponse(Response response) {
+            ToponymObjectMetadata toponymObjectMetadata = response.getCollection().getChildren().stream()
+                    .map(child -> child.getObj().getMetadataContainer().getItem(ToponymObjectMetadata.class))
+                    .filter(Objects::nonNull)
+                    .findFirst()
+                    .orElse(null);
+
+            String street = "Информация об улице не найдена";
+
+            if (toponymObjectMetadata != null) {
+                street = toponymObjectMetadata.getAddress().getComponents().stream()
+                        .filter(component -> component.getKinds().contains(Address.Component.Kind.STREET))
+                        .map(Address.Component::getName)
+                        .findFirst()
+                        .orElse("Информация об улице не найдена");
+            }
+
+            Toast.makeText(getApplicationContext(), street, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onSearchError(@NonNull Error error) {
+
+        }
+    };
+
+    private final InputListener inputListener = new InputListener() {
+        @Override
+        public void onMapTap(Map map, Point point) {
+            searchSession = searchManager.submit(point, 20, new SearchOptions(), searchListener);
+        }
+
+        @Override
+        public void onMapLongTap(Map map, Point point) {
+            // Обработка долгого нажатия на карту (если нужно)
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,16 +126,9 @@ public class MapActivity extends AppCompatActivity implements
 
             loadUserLocationLayer();
 
-            tapListener = new GeoObjectTapListener() {
-                @Override
-                public boolean onObjectTap(@NonNull GeoObjectTapEvent geoObjectTapEvent) {
-                    GeoObjectSelectionMetadata selectionMetadata = geoObjectTapEvent.getGeoObject()
-                            .getMetadataContainer().getItem(GeoObjectSelectionMetadata.class);
-                    binding.mapView.getMap().selectGeoObject(selectionMetadata.getId(), selectionMetadata.getLayerId());
-                    return false;
-                }
-            };
             binding.mapView.getMap().addTapListener(tapListener);
+            searchManager = SearchFactory.getInstance().createSearchManager(SearchManagerType.ONLINE);
+            binding.mapView.getMap().addInputListener(inputListener);
 
         } else {
             Toast.makeText(this, "Даны не все разрешения", Toast.LENGTH_SHORT).show();
